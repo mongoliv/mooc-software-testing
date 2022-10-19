03e3634a3fccfea159303c7a8f0375417200ca4c
Ivan Bykov <bivmongol@gmail.com>
2022-10-19 17:22:04 +0300

    two tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,24 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.chocolate.ChocolateBags;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "str{0}, result={1}")
+    @CsvSource({"xxggxxx, true", "ggxxxx, true", "xxxxxxgg, true"})
+    public void twoGNormal(String str, boolean expectedResult) {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @ParameterizedTest(name = "str{0}, result={1}")
+    @CsvSource({"xxggxxgx, false", "gxxxggxgg, false", "xxgxxxxgg, false", "gxxxxxxxg, false"})
+    public void fewGNotNormal(String str, boolean expectedResult) {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
